6eb0e881815c4b0b61684e6fa6c66f3eb721a522
Steven Inouye <steveninouye@msn.com>
2017-12-27 21:16:15 -1000

    finishedProduct

--- calculator.js
+++ calculator.js
@@ -5,6 +5,9 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+function calculatorModule (){
+    var _total = 0;
+    var _memory = 0;
 
 
   /**
@@ -12,54 +15,100 @@
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+function load(x){
+    validate(x);
+    _total = x;
+    return x;
+}
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+function getTotal(){
+    return _total;
+}
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
-
+function add(x){
+    validate(x);
+    _total += x;
+    return _total;
+}
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+function subtract(x){
+    validate(x);
+    _total -= x;
+    return _total;
+}
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+function multiply(x){
+    validate(x);
+    _total *= x;
+    return _total;
+}
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+function divide(x){
+    validate(x);
+    _total /= x;
+    return _total;
+}
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+function recallMemory(){
+    return _memory;
+}
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+function saveMemory(){
+    _memory = _total;
+}
 
   /**
    * Clear the value stored at `memory`
    */
+function clearMemory(){
+    _memory = 0;
+}
 
   /**
    * Validation
    */
+function validate(x){
+    if(typeof x !== 'number'){
+        throw Error('Invalid input');
+    }
+}
 
+   return {
+       load: load,
+       getTotal: getTotal,
+       add: add,
+       subtract: subtract,
+       multiply: multiply,
+       divide: divide,
+       recallMemory: recallMemory,
+       saveMemory: saveMemory,
+       clearMemory: clearMemory
+   };
+}
